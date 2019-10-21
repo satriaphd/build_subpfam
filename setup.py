56ffa5f37632ba61e6121758a22cd8e40349525f
Satria A Kautsar <satriaphd@gmail.com>
2019-10-21 11:54:24 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="build_subpfam",
+    version="0.1",
+    author="Satria A Kautsar",
+    author_email="satriaphd@gmail.com",
+    description="Build clade HMMs from a multiple sequence alignment file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/satriaphd/build_subpfam",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "ete3"
+    ]
+)
